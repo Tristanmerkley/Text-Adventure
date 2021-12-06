4db76a4cf32dc8e9607cb1a5c9534d4c6e92f986
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-12-06 17:55:21 -0500

    ~ Fixed DisplayInventory ~ Changed toString

--- Clue/src/zork/Item.java
+++ Clue/src/zork/Item.java
@@ -21,7 +21,7 @@ public class Item extends OpenableObject {
   }
 
   public void displayInventory() {
-    System.out.println(inventory);
+    inventory.displayInventory();
   }
 
   public String getInventory() {
@@ -29,7 +29,7 @@ public class Item extends OpenableObject {
   }
 
   public String toString() {
-    return name + ":" + description;
+    return name;
   }
 
   public int getWeight() {
@@ -66,6 +66,10 @@ public class Item extends OpenableObject {
 
   public void addItem(Item item) {
     inventory.addItem(item);
+  }
+
+  public String getDescription() {
+    return description;
   }
 
 }
