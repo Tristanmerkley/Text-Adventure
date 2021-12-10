1dd44496ef9fa909fe405e29c66a90dabc685ec9
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-12-10 13:09:48 -0500

    ~ Formatting + dropItem return

--- Clue/src/zork/Game.java
+++ Clue/src/zork/Game.java
@@ -47,29 +47,18 @@ public class Game {
       Item item = new Item();
       String itemName = (String) ((JSONObject) itemObj).get("name");
       String itemId = (String) ((JSONObject) itemObj).get("id");
-      long weight = ((JSONObject) itemObj).get("weight") != null ? (Long) ((JSONObject) itemObj).get("weight")
-          : Integer.MAX_VALUE;
+      long weight = ((JSONObject) itemObj).get("weight") != null ? (Long) ((JSONObject) itemObj).get("weight") : Integer.MAX_VALUE;
       long holdingWeight; // ! how much an item can hold in its inventory
-      boolean isLocked = ((JSONObject) itemObj).get("isLocked") != null
-          ? (Boolean) ((JSONObject) itemObj).get("isLocked")
-          : false;
-      boolean isOpenable = ((JSONObject) itemObj).get("isOpenable") != null
-          ? (Boolean) ((JSONObject) itemObj).get("isOpenable")
-          : false;
+      boolean isLocked = ((JSONObject) itemObj).get("isLocked") != null ? (Boolean) ((JSONObject) itemObj).get("isLocked") : false;
+      boolean isOpenable = ((JSONObject) itemObj).get("isOpenable") != null ? (Boolean) ((JSONObject) itemObj).get("isOpenable") : false;
       if (!isOpenable)
         holdingWeight = 0;
       else
-        holdingWeight = ((JSONObject) itemObj).get("holdingWeight") != null
-            ? (Long) ((JSONObject) itemObj).get("holdingWeight")
-            : Long.MAX_VALUE;
+        holdingWeight = ((JSONObject) itemObj).get("holdingWeight") != null ? (Long) ((JSONObject) itemObj).get("holdingWeight") : Long.MAX_VALUE;
 
       String itemDescription = (String) ((JSONObject) itemObj).get("description");
-      String startingRoom = ((JSONObject) itemObj).get("startingroom") != null
-          ? (String) ((JSONObject) itemObj).get("startingroom")
-          : null;
-      String startingItem = ((JSONObject) itemObj).get("startingitem") != null
-          ? (String) ((JSONObject) itemObj).get("startingitem")
-          : null;
+      String startingRoom = ((JSONObject) itemObj).get("startingroom") != null ? (String) ((JSONObject) itemObj).get("startingroom") : null;
+      String startingItem = ((JSONObject) itemObj).get("startingitem") != null ? (String) ((JSONObject) itemObj).get("startingitem") : null;
 
       item.setDescription(itemDescription);
       item.setName(itemName);
@@ -181,16 +170,14 @@ public class Game {
     String commandWord = command.getCommandWord().toLowerCase();
     if (commandWord.equals("help"))
       printHelp();
-    else if (commandWord.equals("south") || commandWord.equals("north") || commandWord.equals("east")
-        || commandWord.equals("west"))
+    else if (commandWord.equals("south") || commandWord.equals("north") || commandWord.equals("east") || commandWord.equals("west"))
       goRoom(command);
     else if (commandWord.equals("quit")) {
       if (command.hasSecondWord())
         System.out.println("Quit what?");
       else
         return true; // signal that we want to quit
-    } else if (commandWord.equalsIgnoreCase("eat") || commandWord.equalsIgnoreCase("drink")
-        || commandWord.equalsIgnoreCase("consume")) {
+    } else if (commandWord.equalsIgnoreCase("eat") || commandWord.equalsIgnoreCase("drink") || commandWord.equalsIgnoreCase("consume")) {
       consumeItem();
     } else if (commandWord.equalsIgnoreCase("inventory")) {
       printInventory();
@@ -227,8 +214,7 @@ public class Game {
       System.out.println("Which direction is the door you want to unlock?");
       return;
     }
-    if (!(direction.equalsIgnoreCase("south") || direction.equalsIgnoreCase("north")
-        || direction.equalsIgnoreCase("west") || direction.equalsIgnoreCase("east"))) {
+    if (!(direction.equalsIgnoreCase("south") || direction.equalsIgnoreCase("north") || direction.equalsIgnoreCase("west") || direction.equalsIgnoreCase("east"))) {
       System.out.println(command.getSecondWord() + " is not a vaild direction");
       return;
     }
@@ -303,6 +289,7 @@ public class Game {
     }
     if (playerInventory.contains(command.getSecondWord()) == null) {
       System.out.println("You do not have a " + command.getSecondWord());
+      return;
     }
     Item item = playerInventory.removeItem(command.getSecondWord());
     currentRoom.addItem(item);
