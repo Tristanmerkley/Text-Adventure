bb5533991dc12c7310ea56843bece6f34942bd56
Tristan Merkley <tmerkley@bayviewglen.ca>
2021-12-13 13:42:47 -0500

    + thirdWord Funtionality

--- Clue/src/zork/Parser.java
+++ Clue/src/zork/Parser.java
@@ -21,14 +21,17 @@ public class Parser {
 
     words = inputLine.split(" ", 2);
     String word1 = words[0];
-    String word2 = null;
-    if (words.length > 1)
-      word2 = words[1];
+    words = words.length > 1 ? words[1].split(" in ") : null;
+    String word2 = words != null ? words[0] : null;
+    String word3 = words != null && words.length > 1 ? words[1] : null;
 
+    if (word3 != null && commands.isCommand(word1))
+      return new Command(word1, word2, word3);
     if (commands.isCommand(word1))
       return new Command(word1, word2);
     else
       return new Command(null, word2);
+
   }
 
   /**
