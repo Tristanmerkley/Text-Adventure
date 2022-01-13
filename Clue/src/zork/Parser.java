2820c05b10e41d46f56ec841639812b138dddc54
Tristan Merkley <tmerkley@bayviewglen.ca>
2022-01-13 16:05:57 -0500

    + Remaining Time

--- Clue/src/zork/Parser.java
+++ Clue/src/zork/Parser.java
@@ -1,5 +1,6 @@
 package zork;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class Parser {
@@ -14,8 +15,11 @@ public class Parser {
   public Command getCommand() throws java.io.IOException {
     String inputLine = "";
     String[] words;
-
-    System.out.print("> "); // print prompt
+    double timeLeft = (Game.MAX_ALLOWED_TIME - (Game.timeElapsed + (new Date().getTime() - Game.startTime) / 1000.0));
+    System.out.printf("\033[1;91m" + "Minutes Remaining: " + "%.0f", timeLeft / 60); // prints minutes left using red font
+    if (timeLeft % 60 != 0)
+      System.out.printf(":" + "%2.0f", timeLeft % 60); // prints out seconds remaing when more than 0 seconds
+    System.out.print("\n\u001B[0m> "); // print prompt
 
     inputLine = in.nextLine();
 
