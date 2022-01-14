e376ffc58a2a1bd79cd01377e88068e2c45b5d0a
Veronica Rodovinsky <vrodovinsky@bayviewglen.ca>
2022-01-14 13:33:12 -0500

    started comments

--- Clue/src/zork/Command.java
+++ Clue/src/zork/Command.java
@@ -6,9 +6,9 @@ public class Command {
   private String thirdWord;
 
   /**
-   * Create a command object. First and second word must be supplied, but either
-   * one (or both) can be null. The command word should be null to indicate that
-   * this was a command that is not recognised by this game.
+   * Create a command object. First and second word must be supplied, but either one (or both) can be
+   * null. The command word should be null to indicate that this was a command that is not recognised
+   * by this game.
    */
   public Command(String firstWord, String secondWord) {
     commandWord = firstWord;
@@ -22,16 +22,15 @@ public class Command {
   }
 
   /**
-   * Return the command word (the first word) of this command. If the command was
-   * not understood, the result is null.
+   * Return the command word (the first word) of this command. If the command was not understood, the
+   * result is null.
    */
   public String getCommandWord() {
     return commandWord;
   }
 
   /**
-   * Return the second word of this command. Returns null if there was no second
-   * word.
+   * Return the second word of this command. Returns null if there was no second word.
    */
   public String getSecondWord() {
     if (CommandWords.dirConversions.get(secondWord) != null)
@@ -57,10 +56,17 @@ public class Command {
     return CommandWords.directions.contains(commandWord); // returns true or false
   }
 
+
+  /**
+   * Return true if the command has a third word.
+   */
   public boolean hasThirdWord() {
     return (thirdWord != null);
   }
 
+  /**
+   * Return the third word of this command. Returns null if there was no third word.
+   */
   public String getThirdWord() {
     return thirdWord;
   }
