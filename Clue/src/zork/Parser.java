2dc899782f0648243da0d19d7f032781a0fbb446
Tristan Merkley <tmerkley@bayviewglen.ca>
2022-01-17 16:04:41 -0500

    ~ Time Remaining Format

--- Clue/src/zork/Parser.java
+++ Clue/src/zork/Parser.java
@@ -22,8 +22,7 @@ public class Parser {
     String[] words;
     double timeLeft = (Game.MAX_ALLOWED_TIME - (Game.timeElapsed + (new Date().getTime() - Game.startTime) / 1000.0));
     System.out.print("\033[1;91m" + "Time Remaining: " + (int) timeLeft / 60); // prints minutes left using red font
-    if (timeLeft % 60 != 0)
-      System.out.printf(":" + "%02.0f", timeLeft % 60); // prints out seconds remaing when more than 0 seconds
+    System.out.printf(":" + "%02.0f", timeLeft % 60); // prints out seconds remaing when more than 0 seconds
     System.out.print("\n\u001B[0m> "); // print prompt
 
     inputLine = in.nextLine();
